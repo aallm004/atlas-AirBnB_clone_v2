a49b6aea3a7ba9482f00039ec6650f03abeceaf5
aallm004 <abigailrosea10@gmail.com>
2024-06-27 15:51:16 -0500

    corrected spelling error and added missing ; to mysql files

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,5 @@
 -- Creating database
 CREATE DATABASE IF NOT EXISTS hbnh_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbmb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
-
