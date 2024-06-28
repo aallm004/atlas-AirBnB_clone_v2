42d699860496455a05b2b07977587edc328667b5
aallm004 <abigailrosea10@gmail.com>
2024-06-28 16:51:44 -0500

    changed import for uuid

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import models
-from uuid import uuid4
+import uuid
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
