a6f5a27dc4165ed41e6d4868561893a3bef2fff8
aallm004 <abigailrosea10@gmail.com>
2024-06-28 16:00:12 -0500

    updated imports in city.py

--- models/city.py
+++ models/city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy import Column, string, ForeignKey
+from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel
 from models.state import State
 
