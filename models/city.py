7357d5e2d676041c43437cbb4e5846c8311e31be
jaylenperez <jaylenestella24@gmail.com>
2024-06-29 18:30:09 -0500

    added comments to explain what I added

--- models/city.py
+++ models/city.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy import Column, string, ForeignKey
-from models.base_model import BaseModel
+from sqlalchemy.orm import relationship # Import the relationship function from SQLAlchemy to define relationships between models
+from sqlalchemy import Column, String, ForeignKey # Import needed classes from SQLAlchemy to define schema of the City model.
+from models.base_model import BaseModel, Base
 from models.state import State
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
+    # Define name of the table in the database that will store City records.
+    __tablename__ = 'cities'
+
+    # Define column for the name of the city. The name is the string of up to 128 characters and cannot be null.
+    name = Column(String(128), nullable=False)
+    # Define attribute for the state ID. This will be replaced with a proper column definition
     state_id = ""
-    name = ""
+    # Define relationship between the City model and the Place model.
+    # 'places' attribute will store a list of Place objects associated with this City.
+    # 'backref' attribute ensures that each Place object will have a reference to its City via the 'cities' attribute.
+    # The 'cascade' attribute ensures that all Place objects linked to a City will be automatically deleted if the City is deleted.
+    places = relationship('Place', backref='cities', cascade='all, delete-orphan')
--- models/place.py
+++ models/place.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+# Imports necessary SQLAlchemy classes for defining database schema and data types.
 from sqlalchemy import (Column, String, ForeignKey, float, Integer)
 from models.base_model import BaseModel, Base
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
+    # Defines name of table in the database that will store Place records.
     __tablename__ = 'places'
+
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
