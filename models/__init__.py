4adc5e0115970d6f51703ea30f9ecfe5489c832a
aallm004 <abigailrosea10@gmail.com>
2024-07-01 10:45:43 -0500

    changes to user.py to accomodate os environment

--- models/__init__.py
+++ models/__init__.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
 import os
-from models.engine.file_storage import FileStorage
 
-#Determine the storage type based on the environment variable
-storage_type = os.getenv("HBNB_TYPE_STORAGE")
-
-if storage_type == "db":
-    # Imports DBStorage class
+if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
-    # Creates an instance of DBStorage
     storage = DBStorage()
-
 else:
-    # Import FileStorage class
     from models.engine.file_storage import FileStorage
-    # Creates an instance of FileStorage
     storage = FileStorage()
 
-# Reload data from storage
 storage.reload()
--- models/user.py
+++ models/user.py
@@ -5,25 +5,32 @@ from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy import relationship
+import os
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
     # Defines the name of the table in the database that will store User record
-    __tablename__ = 'users'
-    # represents a column containing a string (128 characters); cant be null
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    # Defines a relationship between User model and the Place model
-    # The 'places' attribute will store a list of Place objects associated
-    # with this User.
-    # The 'backref' attribute ensures that each Place object will have a
-    # reference to its User via the 'User' attribute.
-    # The 'cascade' attribute ensures that all Place objects linked to a
-    # User will be automatically deleted if the User is deleted.
-    places = relationship('Place', backref='user',
-                          cascade='all, delete-orphan')
-    reviews = relationship('Review', backref='user',
-                           cascade='all, delete-orphan')
+        __tablename__ = 'users'
+        # represents a column containing a string (128 characters); cant be null
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        # Defines a relationship between User model and the Place model
+        # The 'places' attribute will store a list of Place objects associated
+        # with this User.
+        # The 'backref' attribute ensures that each Place object will have a
+        # reference to its User via the 'User' attribute.
+        # The 'cascade' attribute ensures that all Place objects linked to a
+        # User will be automatically deleted if the User is deleted.
+        places = relationship('Place', backref='user',
+                               cascade='all, delete-orphan')
+        reviews = relationship('Review', backref='user',
+                               cascade='all, delete-orphan')
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
