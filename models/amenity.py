71a2805830e1b4638e0034dfbc0f58ef1c0409d0
aallm004 <abigailrosea10@gmail.com>
2024-07-01 17:12:54 -0500

    console.py testing

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,15 @@
-#!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
+from models.base_model import BaseModel, Base, storage_type
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Representation of Amenity """
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
