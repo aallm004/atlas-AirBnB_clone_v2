7fcfe9bc3028963e6bd762900c175343089bdffd
jaylenperez <jaylenestella24@gmail.com>
2024-07-01 00:05:47 -0500

    changed secondary to place_amenities

--- models/place.py
+++ models/place.py
@@ -26,7 +26,7 @@ class Place(BaseModel, Base):
     longitude = Column(float)
     reviews = relationship('Review', backref='place',
                            cascade='all, delete-orphan')
-    amenities = relationship('Amenity', secondary='place',
+    amenities = relationship('Amenity', secondary='place_amenities',
                            cascade='all, delete-orphan')
     amenity_ids = []
 
