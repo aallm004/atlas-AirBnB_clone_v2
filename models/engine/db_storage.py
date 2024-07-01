e08ef6fe0e6f0811af699d347f8008df6d2d4645
aallm004 <abigailrosea10@gmail.com>
2024-07-01 14:58:04 -0500

    pycode style db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -16,14 +16,15 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 classes = {"Amenity": Amenity, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
+
 """DBStorage class for interacting with MySQL database"""
 class DBStorage:
     """what do"""
     __engine = None
     __session = None
     __file_storagee = FileStorage()
-    
-    
+
+
     def __init__(self):
         """Initialize DBStorage"""
         self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,7 +29,7 @@ class FileStorage():
         stores all objects managed by the instance "FileStorage"."""
         if not cls:
             return self.__objects
-        elif type(cls) == str:
+        elif type(cls) is str:
             return {k: v for k, v in self.__objects.items()
                     if v.__class__.__name__ == cls}
         else:
