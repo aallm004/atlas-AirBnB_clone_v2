de3532363f87464ae6d94b332cf72ccf32999d6b
aallm004 <abigailrosea10@gmail.com>
2024-07-02 11:02:17 -0500

    added import unittest to user and city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -2,6 +2,8 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
+import unittest
+from models import storage_type
 
 
 class test_City(test_basemodel):
