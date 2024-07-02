396da645d7e4258bea6bd44d97ad2ddfa4b6410d
aallm004 <abigailrosea10@gmail.com>
2024-07-02 13:01:03 -0500

    pycode in db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -25,12 +25,12 @@ class DBStorage:
 
     def __init__(self):
         """Initialize DBStorage"""
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format( \
-                                      os.environ['HBNB_MYSQL_USER'],
-                                      os.environ['HBNB_MYSQL_PWD'],
-                                      os.environ['HBNB_MYSQL_HOST'],
-                                      os.environ['HBNB_MYSQL_DB'])
-                                      pool_pre_ping=True)
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}". \
+                                      format(os.environ['HBNB_MYSQL_USER'],
+                                             os.environ['HBNB_MYSQL_PWD'],
+                                             os.environ['HBNB_MYSQL_HOST'],
+                                             os.environ['HBNB_MYSQL_DB'])
+                                             pool_pre_ping=True)
         if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
