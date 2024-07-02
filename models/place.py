4399f3d0c87ba1219c3be81ccba7222612b786bc
aallm004 <abigailrosea10@gmail.com>
2024-07-02 10:28:59 -0500

    updated tests

--- models/place.py
+++ models/place.py
@@ -10,10 +10,12 @@ from models.review import Review
 from models.amenity import Amenity
 
 association_table = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60), ForeignKey('places.id'), 
-                          primary_key=True, nullable=False),
-                          Column('amenity_id', String(60), ForeignKey('amenities.id'), 
-                                primary_key=True, nullable=False))
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
