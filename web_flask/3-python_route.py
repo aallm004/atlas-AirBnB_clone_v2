6e8b8e126c759d66c1c643c23f6156aab1c1f0cd
aallm004 <abigailrosea10@gmail.com>
2024-07-06 13:00:31 -0500

    pep8 missing extra lines

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,10 +22,12 @@ def display_hbnb():
 def display_c(text):
     return "C {}".format(text.replace('_', ' '))
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def display(text="is cool"):
     return "Python {}".format(text.replace('_', ' '))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
