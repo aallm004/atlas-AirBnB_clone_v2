8b309ec3ddac0d0e2f391d29d70dda0735bfa8c5
aallm004 <abigailrosea10@gmail.com>
2024-07-08 17:17:56 -0500

    tennnn

---
+++ web_flask/9-states.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+@app.route('/states', strict_slashes=False)
+def states():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda s: s.name)
+
+    return render_template('states.html', states=sorted_states)
+
+@app.route('/states/<int:id>', strict_slashes=False)
+def state_cities(id):
+    state = storage.get(State, id)
+    if state:
+        cities = sorted(state.cities, key=lambda c: c.name)
+        return render_template('state_cities.html', state=state, cities=cities)
+    else:
+        return '<h1>Not found!</h1>'
+
+@app.teardown_appcontext
+def close_session(exception=None):
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
