5f3acad6c892e68a7132546362355754df80ad3d
aallm004 <abigailrosea10@gmail.com>
2024-07-08 17:05:19 -0500

    task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda s: s.name)
+
+    return render_template('cities_by_states.html', states=sorted_states)
+
+@app.teardown_appcontext
+def close_session(exception=None):
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
