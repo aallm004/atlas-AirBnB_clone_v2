331987ee9614f5189fd39752309bd0c31662aed1
aallm004 <abigailrosea10@gmail.com>
2024-07-08 16:10:27 -0500

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(exception=None):
+    """Remove the current SQLAlchemy session after each request."""
+    storage.close()
+
+@app.route('/states_list', strict_slashes=False)
+def display_states():
+    """Display a list of State objects sorted by name."""
+    states = storage.all("State").values()
+    sorted_states = sorted(states, key=lambda s: s.name)
+
+    return render_template('states_list.html', states=sorted_states)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
